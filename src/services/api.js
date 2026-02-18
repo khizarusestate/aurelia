a616fd2460219d767cce1b594de6d8ba23e41b4a
khizarusestate <khizarusestate@gmail.com>
2026-02-18 22:01:32 +0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://aurelia-server.vercel.app/api';
+const API_BASE_URL = 'https://vercel-server-ten-mauve.vercel.app/api';
 
 export const api = {
   async sendBookingRequest(bookingData) {
